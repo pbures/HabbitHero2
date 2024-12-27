9073b5a10d7353c3836bb692e3a3c28d89589897
Pavel Bureš <pavel.bures@gmail.com>
2024-12-27 22:23:24 +0100

    Top header created

---
+++ src/components/TopHeader.vue
@@ -0,0 +1,59 @@
+<template>
+    <header class="header">
+        <h1 class="title">Habbit Hero</h1>
+        <h1>{{  userStore.name }}</h1>
+        <nav class="nav">
+            <ul>
+                <li v-if="isAuthenticated">
+                    <h1 @click="logout()">Logout</h1>
+                </li>
+                <li v-if="!isAuthenticated">
+                    <button @click="loginWithRedirect"><h1>Login</h1></button>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+    import { useUserStore } from '@/stores/user'
+    import { useAuth0 } from '@auth0/auth0-vue'
+
+    const userStore = useUserStore()
+    const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0()
+
+    userStore.fetchUserData()
+</script>
+
+<style scoped>
+.header {
+    background-color: var(--vt-c-black-mute);
+    max-height: var(--top-header-height);
+    color: white;
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 1000;
+}
+
+.nav ul {
+    list-style: none;
+    display: flex;
+    gap: 1rem;
+    padding: 0;
+    margin: 0;
+}
+
+.nav a {
+    color: white;
+    text-decoration: none;
+}
+
+.nav a:hover {
+    text-decoration: underline;
+}
+</style>
