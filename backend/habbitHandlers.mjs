4dc1c4f0c90fcc3690cd965a554509fc03844a90
Adam Bures <adam.bures.prg@gmail.com>
2025-04-02 00:21:52 +0200

    added test to habbit_invite

--- backend/habbitHandlers.mjs
+++ backend/habbitHandlers.mjs
@@ -1,5 +1,6 @@
 import ajv from 'ajv';
 import Task from '../frontend/src/model/task.mjs';
+import { ObjectId } from 'mongodb';
 
 function useHabbitHandlers(app, checkJwt, myMongoDBManager) {
 
@@ -65,10 +66,12 @@ function useHabbitHandlers(app, checkJwt, myMongoDBManager) {
   app.put('/habbit_invite', checkJwt, async (req, res) => {
     console.log('PUT request at /habbit_invite with body:', req.body, "query:", req.query);
     const userId = req.auth.payload.sub
-    const requestedUserId = req.query.friend_id;
+    const requestedUserId = req.body.friend_id;
     const habbit = req.body.habbit;
-    await myMongoDBManager.push({ _id: habbit._id}, userId, 'user_ids');
-    await myMongoDBManager.push({ _id: habbit._id }, requestedUserId, 'user_ids');
+    // await myMongoDBManager.push({ _id: new ObjectId(habbit._id)}, userId, 'user_ids');
+    await myMongoDBManager.push({ _id: new ObjectId(habbit._id) }, requestedUserId,'observer_ids');
+
+    res.status(200).json({ message: 'Habbit invited successfully' });
   });
 }
 
